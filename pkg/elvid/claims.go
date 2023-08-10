16b8eb72a9a81d56c7b5615d9c1fdd87bec90be3
fuglesang <andreas.fuglesang@gmail.com>
2023-08-10 09:34:08 +0200

    FENIX-825: added missing doc

--- pkg/elvid/claims.go
+++ pkg/elvid/claims.go
@@ -10,11 +10,15 @@ var (
 	ErrEmptyClientID = errors.New("the clientid is empty")
 )
 
+// Claims is an interface that represents the claims fields in a JWT.
+// There is a method Validate that is called during the authentication process.
 type Claims interface {
 	jwt.Claims
 	Validate() error
 }
 
+// StandardClaims contains required claims in a JWT that is used for validation.
+// It also contains claims that is standard for ElvID JWTs, eg Scope.
 type StandardClaims struct {
 	jwt.RegisteredClaims
 	Scope      []string `json:"scope,omitempty"`
@@ -22,6 +26,7 @@ type StandardClaims struct {
 	ClientName string   `json:"client_name,omitempty"`
 }
 
+// Validate validates the parsed claims in a JWT.
 func (c StandardClaims) Validate() error {
 	if c.ClientID == "" {
 		return ErrEmptyClientID
